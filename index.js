18f9005644b3d4fc0ff50f220d0c409d1ef6ac36
bakhti GOUBA <gbakhti5@gmail.com>
2024-09-28 16:16:34 +0100

    feat(create-http-server): create an http server listening on port 3000
    create an http server listening on port 3000
    
    BREAKING CHANGE: the old command line is deleted(console.log)

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-/*const fs = require("fs")*/
+/*const fs = require("fs")
 import fs from "fs";
 const filecontent = fs.readFileSync("./package.json")
-console.log(filecontent.toString());
+console.log(filecontent.toString());*/
+
+import http from "http"
+const server= http.createServer((req, res)=>{
+res.writeHead(200,{"content-type":"text/plain"});
+res.end("HELLO WOLRD");
+
+
+});
+server.listen(3000,()=> {
+    console.log("server is running on port 3000");
+});
